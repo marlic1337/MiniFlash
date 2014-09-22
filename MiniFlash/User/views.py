b6fa16da608e6f3be3ada74b5634528550db1951
nejc92 <nejc.il92@gmail.com>
2014-09-22 14:22:49 +0200

    user settings

--- MiniFlash/User/views.py
+++ MiniFlash/User/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
+from forms import UserSettingsForm
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect
+from models import CustomUser
+
 
 @login_required
 def user_settings(request):
+	if request.method == "POST":
+		form = UserSettingsForm(request.POST, instance=request.user)
+		if form.is_valid():
+			form.save(request)
+			return HttpResponseRedirect(reverse('user_settings'))
+	else:
+		form = UserSettingsForm(instance=request.user)
 	return render_to_response('authenticated/user/user_settings.html', locals(), context_instance=RequestContext(request))
