8e93da58ff515eae3bbaeb72e6b582776cd40103
nejc92 <nejc.il92@gmail.com>
2014-09-22 17:22:31 +0200

    create project

--- MiniFlash/Project/urls.py
+++ MiniFlash/Project/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import url, patterns
 
 urlpatterns = patterns('Project.views',
     url(r'^projects$', 'my_projects', name='my_projects'),
+    url(r'^projects/add$', 'add_project', name='add_project'),
 )
--- MiniFlash/Project/views.py
+++ MiniFlash/Project/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render_to_response
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
+from forms import ProjectForm
+from django.http import HttpResponseRedirect
+from django.contrib import messages
+from django.core.urlresolvers import reverse
+from models import Project
 
 @login_required
 def my_projects(request):
+	my_projects = Project.objects.filter(participants__id__exact=request.user.id)
 	return render_to_response('authenticated/project/my_projects.html', locals(), context_instance=RequestContext(request))
+
+@login_required
+def add_project(request):
+	if request.method == "POST":
+		form = ProjectForm(request.POST, user=request.user)
+		if form.is_valid():
+			form.save(request)
+			messages.success(request, 'Project created successfully.')
+			return HttpResponseRedirect(reverse('my_projects'))
+	else:
+		form = ProjectForm()
+	return render_to_response('authenticated/project/add_project.html', locals(), context_instance=RequestContext(request))
