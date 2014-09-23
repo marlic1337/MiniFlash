3df17762d5ab2d86726486e86bacd10834f9c9f0
nejc92 <nejc.il92@gmail.com>
2014-09-23 22:36:30 +0200

    social links

--- MiniFlash/User/models.py
+++ MiniFlash/User/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import AbstractUser
 from django.conf import settings
 
 class CustomUser(AbstractUser):
-    short_description = models.CharField(max_length=128, blank=True)
+    title = models.CharField(max_length=128, blank=True)
 
     logo = models.ImageField(upload_to='logos', width_field='logo_width', height_field='logo_height', null=True, blank=True)
     logo_width = models.PositiveIntegerField(null=True)
@@ -13,3 +13,12 @@ class CustomUser(AbstractUser):
         if self.logo:
             return self.logo.url
         return settings.STATIC_URL + 'img/default_user.png'
+
+class SocialNetworks(models.Model):
+    user = models.OneToOneField(CustomUser, primary_key=True)
+
+    url_facebook = models.CharField(max_length=64, blank=True)
+    url_googleplus = models.CharField(max_length=64, blank=True)
+    url_twitter = models.CharField(max_length=64, blank=True)
+    url_linkedin = models.CharField(max_length=64, blank=True)
+    url_github = models.CharField(max_length=64, blank=True)
